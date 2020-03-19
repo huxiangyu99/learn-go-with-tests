088beeaa0eb0a2d8fa10d3650b6dc535bb08c9ee
huxy99 <huxy99@126.com>
2020-03-20 02:24:46 +0800

    reflection nil ptr

---
+++ reflection/v9/reflection.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"reflect"
+)
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Ptr:
+		if val.IsNil() {
+			return
+		}
+		val = val.Elem()
+		walkValue(val)
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	return val
+}
